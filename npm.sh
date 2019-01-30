7d412d59f823646f72b6d9e022cd657994bbb997
Zell Liew <zellwk@gmail.com>
2019-01-30 16:09:37 +0800

    add np to npm.sh

--- npm.sh
+++ npm.sh
@@ -2,10 +2,10 @@
 sudo chown -R $USER:$(id -gn $USER) $HOME/.config
 
 # Install commonly used clis
-sudo npm i -g \
-	fkill-cli \
-	http-server \
-	npm-check \
-	standard \
-	svgo \
-	trash-cli \
+sudo npm i -g fkill-cli
+sudo npm i -g http-server
+sudo npm i -g np
+sudo npm i -g npm-check
+sudo npm i -g standard
+sudo npm i -g svgo
+sudo npm i -g trash-cli
