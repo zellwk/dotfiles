a3be8184a4d1af6065406e20d186b28b5b45bf1b
Zell Liew <zellwk@gmail.com>
2019-10-10 12:07:38 +0800

    Remove accidental symlink of .git folder

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 function doIt() {
 	# Make a symlink for all dotfiles in this directory
 	for file in \.[^.]*; do
-		if [ $file != '.DS_Store' ]; then
+		if [ $file != '.DS_Store' ] && [ $file != '.git' ]; then
 			echo "Creating symlink to $file in home directory."
 			ln -sf $PWD/$file ~/$file
 		fi
