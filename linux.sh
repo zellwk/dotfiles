5a9698e805451e9a3670eb3bd3ffa02b4fce36f9
Zell Liew <zellwk@gmail.com>
2020-02-04 10:35:41 +0800

    ADd files to linux installs

--- linux.sh
+++ linux.sh
@@ -1,9 +1,15 @@
-# Install Node
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# Update and upgrade apt
+sudo apt update && sudo apt upgrade -y
 
 # Installs ZSH and Oh my ZSH (Should do this earlier perhaps...)
-apt install zsh -y
+sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-apt install xclip -y
+# ########
+# Binaries
+# ########
+sudo apt install xclip -y
+
+# Node 
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
