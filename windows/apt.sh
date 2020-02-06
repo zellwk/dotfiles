69a0e9172cf7a00521fca906adc7ed1444d0da4a
Zell Liew <zellwk@gmail.com>
2020-02-06 22:11:11 +0800

    Create Windows instructions

--- windows/apt.sh
+++ windows/apt.sh
@@ -11,10 +11,9 @@ sudo apt install youtube-dl -y
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
-# MongoDB for Ubuntu 18.04. Check 
+# MongoDB for Ubuntu 18.04. 
+# Installation instructions: https://docs.mongodb.com/manual/administration/install-on-linux/
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update
 sudo apt install -y mongodb-org
-
-# Note: Starting MongoDB
