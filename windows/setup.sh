3f3e3701790f4a100b530906e48913c83985f7e6
Zell Liew <zellwk@gmail.com>
2020-02-06 17:50:26 +0800

    Reorder windows order

--- windows/setup.sh
+++ windows/setup.sh
@@ -1,10 +1,8 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Update and upgrade apt
 sudo apt update && sudo apt upgrade -y
 
-# Creates hardlinks for startup scripts
-source link-startup-scripts.sh
 
 # Install binaries via apt
 source windows/apt.sh
@@ -12,8 +10,8 @@ source windows/apt.sh
 # Set npm permissions and install npm binaries
 source shared/npm.sh
 
-# Clone repositories
-source shared/clone-repos.sh
-
-# Create symlinks for dotfiles 
+# Create symlinks for dotfiles
 source link-dotfiles.sh
+
+# Creates hard links for Auto Hot Key startup scripts
+source link-startup-scripts.sh
