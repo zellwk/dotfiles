f8e54e8b8eb106a1d93abc3175ef90217642d9c0
Zell Liew <zellwk@gmail.com>
2020-03-16 15:51:54 +0800

    Add fast CLI

--- shared/npm.sh
+++ shared/npm.sh
@@ -4,6 +4,7 @@ sudo chown -R $USER:$(id -gn $USER) $HOME/.config
 
 # Install commonly used clis
 echo "Downloading npm clis"
+sudo npm i -g fast-cli
 sudo npm i -g fkill-cli
 sudo npm i -g http-server
 sudo npm i -g npm-check
