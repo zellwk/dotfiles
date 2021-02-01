549e3a7cd4a8677fca7d1895862d44fe9bb6fb9e
Yaakov Saxon <YSaxon@users.noreply.github.com>
2021-02-01 11:34:02 -0500

    Update install-zsh.sh
    A couple of steps (presumably) necessary after installing homebrew:
    brew install zsh
    and the oh-my-zsh install scripts

--- install-zsh.sh
+++ install-zsh.sh
@@ -15,6 +15,8 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   # Installs Oh my ZSH with Homebrew (Mac)
   if [[ $MACHINE == "Mac" ]]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install zsh
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   fi
 
   # Installs Oh my ZSH with Linux
