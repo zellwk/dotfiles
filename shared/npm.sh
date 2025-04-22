b4c84d7d44da9ee3b33406fe3c95f0c96c00401a
Zell Liew <zellwk@gmail.com>
2025-04-22 14:30:51 +0800

    Remove nx

--- shared/npm.sh
+++ shared/npm.sh
@@ -5,12 +5,11 @@ sudo chown -R $USER:$(id -gn $USER) $HOME/.config
 # Install commonly used clis
 echo "Downloading npm clis"
 
-sudo npm i -g fast-cli
-sudo npm i -g fkill-cli
-sudo npm i -g http-server
-sudo npm i -g nodemon
-sudo npm i -g npm-check
-sudo npm i -g nx
-sudo npm i -g standard
-sudo npm i -g svgo
-sudo npm i -g trash-cli
+npm i -g fast-cli
+npm i -g fkill-cli
+npm i -g http-server
+npm i -g nodemon
+npm i -g npm-check
+npm i -g standard
+npm i -g svgo
+npm i -g trash-cli
